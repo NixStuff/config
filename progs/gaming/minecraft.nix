89e8c9650e546fa4b9bf42a801199b2186cd605e
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-09-09 15:17:34 +0200

    feat(steam+minecraft) : added steam and minecraft

--- progs/gaming/minecraft.nix
+++ progs/gaming/minecraft.nix
@@ -8,24 +8,20 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "gaming";
+    name = "minecraft";
     main-repo = "nix";
     branch = "latest";
-    subfolder = "gaming";
-    imports = [
-      "steam"
-      "minecraft"
-    ];
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
-    };
-    settings = {
-      steam.enable = true;
-      minecraft.enable = true;
     };
   };
 in (tools.fullModule rec {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  Home = {
+    home.packages = with packages; [
+      prismlauncher
+    ];
+  };
 })
--- progs/gaming/steam.nix
+++ progs/gaming/steam.nix
@@ -8,24 +8,31 @@
 }: let
   moduleParams = tools.moduleParams rec {
     inherit config lib pkgs-list parentPathAsList tools;
-    name = "gaming";
+    name = "steam";
     main-repo = "nix";
     branch = "latest";
-    subfolder = "gaming";
-    imports = [
-      "steam"
-      "minecraft"
-    ];
     options = {
       enable = lib.mkEnableOption "Enables ${name} program and related settings.";
-    };
-    settings = {
-      steam.enable = true;
-      minecraft.enable = true;
     };
   };
 in (tools.fullModule rec {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+  System = {
+    programs = {
+      gamemode.enable = true;
+      steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+      };
+    };
+    environment.sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/user/.steam/root/compatibilitytools.d";
+    };
+    environment.systemPackages = with packages; [
+      mangohud
+      protonup-ng
+    ];
+  };
 })
