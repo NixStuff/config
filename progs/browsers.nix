9a6922f8b1d8c5f06392909a660152e5f7931727
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-09-16 09:56:03 +0200

    feat(browser) : defined zen as default browser WIP

--- progs/browsers.nix
+++ progs/browsers.nix
@@ -27,19 +27,18 @@
       zen.enable = true;
     };
   };
-in (tools.fullModule rec {
+in (tools.fullModule {
   inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
   inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
   inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
-  # System = {
-  #   environment.sessionVariables.DEFAULT_BROWSER = "${pkgs-list.nix.latest.firefox}/bin/firefox";
-  #   xdg.mime.enable = true;
-  #   xdg.mime.defaultApplications = {
-  #     "text/html" = "firefox.desktop";
-  #     "x-scheme-handler/http" = "firefox.desktop";
-  #     "x-scheme-handler/https" = "firefox.desktop";
-  #     "x-scheme-handler/about" = "firefox.desktop";
-  #     "x-scheme-handler/unknown" = "firefox.desktop";
-  #   };
-  # };
+  System = {
+    # environment.sessionVariables.DEFAULT_BROWSER = "${pkgs-list.others.zen-browser}/bin/zen-browser";
+    xdg.mime.defaultApplications = {
+      # "text/html" = "zen-beta.desktop";
+      "x-scheme-handler/http" = "zen-beta.desktop";
+      "x-scheme-handler/https" = "zen-beta.desktop";
+      "x-scheme-handler/about" = "zen-beta.desktop";
+      "x-scheme-handler/unknown" = "zen-beta.desktop";
+    };
+  };
 })
