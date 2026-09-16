62f702e071e463b1ed36da3d8218b46f2a1cf904
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-09-16 10:12:56 +0200

    update(zsh) : change origin of package to unstable channel + update nix unstable channel => update zsh pkg

--- progs/shells/zsh.nix
+++ progs/shells/zsh.nix
@@ -24,7 +24,7 @@
         default = {};
       };
       p10k.enable = lib.mkEnableOption "Enables powerlevel10k zsh theme.";
-package = lib.mkOption {
+      package = lib.mkOption {
         type = lib.types.package;
         description = "The zsh package to use.";
         default = pkgs-list.nix.latest.zsh;
@@ -44,67 +44,68 @@ package = lib.mkOption {
   };
 in
   (tools.fullModule rec {
-  inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
-  inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
-  inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
-  Home = {
-    home.file.".p10k.zsh".text = "${builtins.readFile "${./${subfolder}/.p10k.zsh}"}";
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      history = {
-        path = "$HOME/.zsh_history";
-        size = 10000;
-        ignoreAllDups = true;
-      };
-      defaultKeymap = "emacs";
-      plugins = [
-        (tools.ifEnabled config "config.progs.shells.zsh.p10k" {
-          name = "powerlevel10k";
-          src = packages.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        })
-        (tools.ifEnabled config "config.progs.shells.zsh.fzf" {
-          name = "fzf-tab";
-          src = "${packages.zsh-fzf-tab}/share/fzf-tab";
-        })
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = packages.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.8.0";
-            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-          };
-        }
-      ];
-      initContent =
-        (tools.ifEnabled config "config.progs.shells.zsh.p10k"
-          "source ~/.p10k.zsh\n")
-        + "${builtins.readFile ./${subfolder}/init.zsh}"
-        + (tools.ifEnabled config "config.progs.shells.zsh.fzf"
-          "zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -a --color $realpath'");
-
-      siteFunctions =
-        {
-          myip = "echo $(ip addr show wlp0s20f3 | grep -oP 'inet \\K[^/]+')";
-        }
-        // (
-          tools.ifEnabled config "config.progs.tuis.yazi" {
-            y = "${builtins.readFile ../tuis/yazi/y.zsh}";
+    inherit (moduleParams) config lib pkgs-list parentPathAsList tools;
+    inherit (moduleParams) name togglable subfolder main-repo branch extras imports specialImports options settings;
+    inherit (moduleParams) packages currentPathAsList currentDirPath cfg inheritedSettings Common;
+    Home = {
+      home.file.".p10k.zsh".text = "${builtins.readFile "${./${subfolder}/.p10k.zsh}"}";
+      programs.zsh = {
+        enable = true;
+        package = pkgs-list.nix.unstable.zsh;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        history = {
+          path = "$HOME/.zsh_history";
+          size = 10000;
+          ignoreAllDups = true;
+        };
+        defaultKeymap = "emacs";
+        plugins = [
+          (tools.ifEnabled config "config.progs.shells.zsh.p10k" {
+            name = "powerlevel10k";
+            src = packages.zsh-powerlevel10k;
+            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+          })
+          (tools.ifEnabled config "config.progs.shells.zsh.fzf" {
+            name = "fzf-tab";
+            src = "${packages.zsh-fzf-tab}/share/fzf-tab";
+          })
+          {
+            name = "zsh-nix-shell";
+            file = "nix-shell.plugin.zsh";
+            src = packages.fetchFromGitHub {
+              owner = "chisui";
+              repo = "zsh-nix-shell";
+              rev = "v0.8.0";
+              sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+            };
           }
-        );
+        ];
+        initContent =
+          (tools.ifEnabled config "config.progs.shells.zsh.p10k"
+            "source ~/.p10k.zsh\n")
+          + "${builtins.readFile ./${subfolder}/init.zsh}"
+          + (tools.ifEnabled config "config.progs.shells.zsh.fzf"
+            "zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -a --color $realpath'");
 
-      shellAliases = cfg.aliases;
+        siteFunctions =
+          {
+            myip = "echo $(ip addr show wlp0s20f3 | grep -oP 'inet \\K[^/]+')";
+          }
+          // (
+            tools.ifEnabled config "config.progs.tuis.yazi" {
+              y = "${builtins.readFile ../tuis/yazi/y.zsh}";
+            }
+          );
+
+        shellAliases = cfg.aliases;
+      };
     };
-  };
-  System = {
-    programs.zsh.enable = true;
-  };
-})
-// {
+    System = {
+      programs.zsh.enable = true;
+    };
+  })
+  // {
     package = pkgs-list.nix.unstable.zsh;
   }
